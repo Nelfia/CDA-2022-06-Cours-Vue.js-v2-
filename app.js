3889b6d7902e8c44434cc3d8237ebdebd6e47017
Nelfia <qdecapele@gmail.com>
2022-09-17 12:47:48 +0200

    Démo - guide vue.js sur le Computed Setter

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-new Vue({
-    el: `#app-6`,
+let vm = new Vue({
+    el: '#demo',
     data: {
-        message: 'Hello Vue.js !'
+        firstName: 'John',
+        lastName: 'Doe',
+    },
+    computed: {
+        fullName: {
+            get: function() {
+                return this.firstName + ' ' + this.lastName
+            },
+            set: function(newValue) {
+                let names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+            }
+        }
     }
 })
+
+
