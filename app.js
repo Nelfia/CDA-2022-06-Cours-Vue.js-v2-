3a419c720c54037ec5235344a779617e557d1fa2
Nelfia <qdecapele@gmail.com>
2022-09-17 15:11:51 +0200

    Ajout de lodash + watch-example du guide vue js 2 en cours

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-let vm = new Vue({
-    el: '#demo',
+let watchExampleVM = new Vue({
+    el: '#watch-example',
     data: {
-        firstName: 'John',
-        lastName: 'Doe',
+        question: "",
+        answer: 'I cannot give you an answer until you ask a question'
     },
-    computed: {
-        fullName: {
-            get: function() {
-                return this.firstName + ' ' + this.lastName
-            },
-            set: function(newValue) {
-                let names = newValue.split(' ')
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
+    watch: {
+        // Lorsque question change, cette fonction est exécutée
+        question: function (newQuestion, oldQuestion) {
+            this.answer = 'Waiting for you to stop typing...'
+            this.debouncedGetAnswer
+        }
+    },
+    created: function () {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+        getAnswer: function () {
+            if (this.question.indexOf('?') === -1) {
+                this.answer = "Questions usually contain a question mark. ;-)"
+                return
             }
+            this.answer = 'Thinking...'
+            let vm = this
+            axios.get('https://yesno.wtf/api')
+            .then(function(response) {
+                vm.answer = _.capitalize(response.data.answer)
+            })
+            .catch(function (error) {
+                vm.answer = 'Error! Could not reach the API. '+ error
+            })
         }
     }
 })
