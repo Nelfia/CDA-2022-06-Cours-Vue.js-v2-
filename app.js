825fe9b41d797b1f665fbe49394467d9419546fe
Nelfia <qdecapele@gmail.com>
2022-09-18 23:17:20 +0200

    Changement du style d'un objet de manière dynamique - OK. RedSkin or GreenSkin?

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-let watchExampleVM = new Vue({
+var watchExampleVM = new Vue({
     el: '#watch-example',
     data: {
-        question: "",
-        answer: 'I cannot give you an answer until you ask a question'
-    },
-    watch: {
-        // Lorsque question change, cette fonction est exécutée
-        question: function (newQuestion, oldQuestion) {
-            this.answer = 'Waiting for you to stop typing...'
-            this.debouncedGetAnswer()
-        }
-    },
-    created: function () {
-        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
-    },
-    methods: {
-        getAnswer: function () {
-            if (this.question.indexOf('?') === -1) {
-                this.answer = "Questions usually contain a question mark. ;-)"
-                return
+        isActive: true,
+        errorClass: 'Not Active !'
+    }, 
+    computed: {
+        classObject: function () {
+            return { active: this.isActive && !this.errorClass}
+        },
+        objectStyle: function () {
+            return {
+                color: this.isActive ? 'green' : "red",
+                backgroundColor: this.isActive ? '#a8c4a824' : '#f443361f',
+                padding: "0.7rem 1rem",
+                borderRadius: "8px"
             }
-            this.answer = 'Thinking...'
-            let vm = this
-            axios.get('https://yesno.wtf/api')
-            .then(function(response) {
-                vm.answer = _.capitalize(response.data.answer)
-            })
-            .catch(function (error) {
-                vm.answer = 'Error! Could not reach the API. '+ error
-            })
         }
     }
 })
