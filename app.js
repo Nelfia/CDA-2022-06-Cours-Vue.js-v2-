6ee70ec83796de8e8d04d21b8e774e9c305d0d45
Nelfia <qdecapele@gmail.com>
2022-09-21 15:18:44 +0200

    "TP2 - Application pour éditer des factures clients: ajout du component totaux (en charge de la gestion et du calcul des totaux) + ajout du component clients (données client) + ajout du numéro de la facture"

--- app.js
+++ app.js
@@ -1,48 +1,149 @@
-let form = new Vue({
-    el: '#form',
-    data: {
-        prenom: '',
-        nom: '',
-        age: '',
-        selectedProvider: '',
-        autreProvider: '',
-        beers: ['Bud', '1664', 'Heineken', 'Grimbergen', 'Faro', 'Belzebuth', 'Guinness', 'Corona', 'Punk-IPA', 'Desperados', 'Delirium'],
-        selectedBeers: [],
-        isAgreeForNewsletter: false,
-        beersListTemplate: [],
-        isValid: 'true'
+let totaux = Vue.component('totaux-item', {
+    props: {
+        'lines': Array,
+        required: true
     },
-    computed: {
-        fullName: function () {
-            return this.prenom + ' ' + this.nom.split('')[0] + '.'
-        },
-        hasFullName: function () {
-            return this.nom && this.prenom
-        },
-        isAutreSelected: function () {
-            return this.selectedProvider == "autre";
+    data() {
+        return {
+            totaux: {
+                "ht": {
+                    name: 'totalHT',
+                    value: 0
+                },
+                "tva": {
+                    name: 'totalTVA',
+                    value: 0
+                },
+                "ttc": {
+                    name: 'totalTTC',
+                    value: 0
+                },
+            }    
+        }
+    },
+    watch: {
+        lines: {
+            handler: function() {
+                this.totaux.ht.value = 0
+                this.totaux.tva.value = 0
+                this.lines.forEach((line) => {
+                    this.totaux.ht.value += line.total
+                    this.totaux.tva.value += line.tva * line.total
+                    this.totaux.ttc.value = this.totaux.ht.value + this.totaux.tva.value
+                });
+            },
+            deep: true
+        }},
+    template: `  
+        <div>
+            <table>
+                <tr v-for="total in totaux">
+                    <td>{{ total.name }}</td>
+                    <td>{{ total.value }}</td>
+                </tr>
+            </table>
+        </div>    
+    `
+})
 
-    }
-},
-methods: {
-    messageConsole: function () {
-        return console.log('Votre inscription est validée !')
-    }
-},
-watch: {
-        beersListTemplate: function () {
-            beersListTemplate.push( `
-                <input type="checkbox" :id="this.beer" :value="this.beer" v-model="selectedBeers">
-                <label :for="this.beer">{{this.beer}}</label><br>
-                `)        },
-        isTxtOk: function (string) {
-            return string.length <= 3
+let client = Vue.component('client-infos', {
+    data(){
+        return {
+            text: 'hello',
+            nomClient: "",
+            prenomClient: "",
+            adresseClient: "",
+            cpClient: 0,
+            villeClient: "",
+            telClient : 0
+        }
+    },
+    template: `
+        <div>
+            <table>
+                <tr>
+                    <td>Nom</td>
+                    <td>
+                        <input type="text" v-model="nomClient"> 
+                    </td>
+                    <td>{{ nomClient }}</td>
+                </tr>
+                <tr>
+                    <td>Prénom</td>
+                    <td>
+                        <input type="text" v-model="prenomClient"> 
+                    </td>
+                    <td>{{ prenomClient }}</td>
+                </tr>
+                <tr>
+                    <td>Adresse</td>
+                    <td>
+                        <input type="text" v-model="adresseClient"> 
+                    </td>
+                    <td>{{ adresseClient }}</td>
+                </tr>
+                <tr>
+                    <td>Code Postal</td>
+                    <td>
+                        <input type="number" v-model="cpClient"> 
+                    </td>
+                    <td>{{ cpClient }}</td>
+                </tr>
+                <tr>
+                    <td>Ville</td>
+                    <td>
+                        <input type="text" v-model="villeClient"> 
+                    </td>
+                    <td>{{ villeClient }}</td>
+                </tr>
+                <tr>
+                    <td>Téléphone</td>
+                    <td>
+                        <input type="number" v-model="telClient"> 
+                    </td>
+                    <td>{{ telClient }}</td>
+                </tr>
+            
+            </table>
+        </div>
+    `
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        lines: [],
+        defaultLine: {
+            'ref': '',
+            'description': '',
+            'quantity': 1,
+            'price': 0,
+            'tva': null,
+            'total': 0
         },
-        isAgeOk: function () {
-            return age > 18
+        billNumber: "F2003002458"
+
+    },
+    created: function () {
+        this.addLine();
+    },
+    methods: {
+        addLine: function () {
+            this.lines.push(Object.assign({}, this.defaultLine))
+        },
+        removeLine: function (index) {
+            this.lines.splice(index, 1)
+        }
+    },
+    watch: {
+        lines: {
+            handler: function () {
+                this.lines.forEach(function (line) {
+                    line.total = line.price * line.quantity;
+                });
+            },
+            deep: true
         }
 
     }
 })
-
-
